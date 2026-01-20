72ee006b0e8f5ae10987f44e3b8e5e311198bcf0
Michael Bany <mobany526@gmail.com>
2026-01-20 10:56:00 +0100

    feat: add configuration check for tracker before sending reports

--- src/Console/Commands/TrackerSendCommand.php
+++ src/Console/Commands/TrackerSendCommand.php
@@ -3,6 +3,7 @@
 namespace Rosalana\Tracker\Console\Commands;
 
 use Illuminate\Console\Command;
+use Rosalana\Core\Facades\App;
 use Rosalana\Tracker\Facades\Tracker;
 
 class TrackerSendCommand extends Command
@@ -13,6 +14,11 @@ class TrackerSendCommand extends Command
 
     public function handle()
     {
+        if (! App::config('tracker.enabled')) {
+            $this->error('Tracker is disabled in the configuration.');
+            return;
+        }
+
         Tracker::sendCaptured();
 
         $this->info('Tracker reports sent.');
