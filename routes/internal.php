b1dd762af5ab6608ed5c947d4905d9c24f1ae2db
Michael Bany <mobany526@gmail.com>
2026-03-22 15:33:08 +0100

    fix: route error

--- routes/internal.php
+++ routes/internal.php
@@ -3,7 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use Rosalana\Tracker\Http\Controllers\TrackerController;
 
-Route::group('tracker', function () {
+Route::group(['prefix' => 'tracker'], function () {
     Route::post('send-captured', [TrackerController::class, 'send'])->name('tracker.send-captured');
     Route::post('flush-captured', [TrackerController::class, 'flush'])->name('tracker.flush-captured');
 });
