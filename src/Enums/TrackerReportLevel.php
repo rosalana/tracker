4132adc5623eb10bbf768bbf18edfcd33ef27c60
Michael Bany <mobany526@gmail.com>
2026-03-28 16:49:56 +0100

    feat: add TrackerReportLevel enum

---
+++ src/Enums/TrackerReportLevel.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Rosalana\Tracker\Enums;
+
+enum TrackerReportLevel: string
+{
+    case DEBUG = 'debug';
+    case INFO = 'info';
+    case NOTICE = 'notice';
+    case WARNING = 'warning';
+    case ERROR = 'error';
+    case CRITICAL = 'critical';
+    case ALERT = 'alert';
+    case EMERGENCY = 'emergency';
+}
